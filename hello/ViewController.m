ab1a1309e8ea0d94ace4154c4bff12d627b59159
Tatu <tatu@TatudeMacBook-Pro.local>
2014-03-11 20:13:34 +0800

    init

--- hello/ViewController.m
+++ hello/ViewController.m
@@ -19,6 +19,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)showAlert:(id)sender {[[[UIAlertView alloc]initWithTitle:@"hello,苍老师" message:@"听说你来中国了" delegate:nil cancelButtonTitle:@"我来看你了" otherButtonTitles:nil, nil]show];
+}
 
 - (void)didReceiveMemoryWarning
 {
